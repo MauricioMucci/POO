375599d7f6e8716f574900636743a994511b450c
MauricioMucci <mauricio_mucci@hotmail.com>
2021-05-17 15:02:40 -0300

    Melhorando o tratamento de exceptions

--- SistemaBanco/SistemaBanco/Conta.java
+++ SistemaBanco/SistemaBanco/Conta.java
@@ -6,12 +6,24 @@ public class Conta {
     private Banco banco;
     private double saldo, limite;
 
+    Conta(){
+
+    }
+
     Conta(String dono, int codigo, Banco banco) {
         this.dono = dono;
         this.codigo = codigo;
         this.banco = banco;
         this.saldo = 0.0;
         this.limite = 1000.0;
+    }
+
+    public Banco getBanco() {
+        return this.banco;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
     }
 
     public int getCodigo() {
@@ -34,16 +46,15 @@ public class Conta {
         this.saldo += valor;
     }
 
-    boolean saca(double valor) {
+    public void saca(double valor) {
         if (ultrapassouLimite(valor)) {
-            throw new SaldoInsuficienteException("\n\t[Limite ultrapassado!]\n");
+            throw new SaldoInsuficienteException("\n\t[Saldo Insuficinete! Saldo atual: " + this.getSaldo() + "]\n");
         } else {
             this.saldo -= valor;
-            return true;
         }
     }
     
-    boolean ultrapassouLimite(double valor) {
+    private boolean ultrapassouLimite(double valor) {
         if (valor <= (this.limite + this.saldo)) {
             return false;
         } else {
@@ -51,11 +62,12 @@ public class Conta {
         }
     }
 
-    void transfere(double valor, Conta conta) {
-        if (saca(valor)) {
-            conta.deposita(valor);
-        } else {
-            System.out.println("Nao eh possivel realizar a transferencia!");
-        }
+    public void transfere(double valor, Conta conta) {
+        saca(valor);
+        conta.deposita(valor);
+    }
+
+    public String toString() {
+        return "Nome: " + this.getDono() + "\nSaldo: " + this.getSaldo();
     }
 }
--- SistemaBanco/SistemaBanco/SistemaBanco.java
+++ SistemaBanco/SistemaBanco/SistemaBanco.java
@@ -1,67 +1,86 @@
 package SistemaBanco;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class SistemaBanco {
     public static void main(String[] args) {
         Scanner scanf = new Scanner(System.in);
 
-        int escolha, codigo;
+        int escolha = 0, codigo = 0;
         String nome;
         Banco banco = new Banco("Itau");
 
         do {
-            System.out.println("-----[ Menu ]-----");
+            System.out.println("\n-----[ Menu ]-----");
             System.out.println("1 - Incluir");
             System.out.println("2 - Remover");
             System.out.println("3 - Listar");
             System.out.println("4 - Acessar sua conta");
             System.out.println("5 - Sair");
             System.out.print("Digite: ");
+            try {
                 escolha = scanf.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("\nErro: O valor digitado não corresponde a um número.\n");
+            } finally {
                 scanf.nextLine();
+            }
             switch (escolha) {
                 case 1:
-                    System.out.print("Qual o nome do correntista?\nR: ");
+                    System.out.print("\nQual o nome do correntista?\nR: ");
                         nome = scanf.nextLine();
                     banco.adicionaConta(nome);
                     break;
                 case 2:
-                    System.out.print("Qual o numero da conta?\nR: ");
+                    System.out.print("\nQual o numero da conta?\nR: ");
                         codigo = scanf.nextInt();
-                    banco.removeConta(banco.procuraConta(codigo));
+                        try {
+                            banco.removeConta(banco.procuraConta(codigo));    
+                        } catch (ContaInexistente e) {
+                            System.out.println(e.getMessage());
+                        }
                     break;
                 case 3:
                     banco.imprimir();
                     break;
                 case 4:
                     try {
-                        System.out.print("Qual o numero da conta?\nR: ");
+                        System.out.print("\nQual o numero da conta?\nR: ");
                             codigo = scanf.nextInt();
                             scanf.nextLine();
+
                         Conta dono = banco.procuraConta(codigo);
                         do {
                             int valor;
-
-                            System.out.println("-----[ Menu ]-----");
+                            
+                            System.out.println("\n--------------------------------");
+                            System.out.println(dono.toString());
+                            System.out.println("--------------------------------");
+                            
+                            System.out.println("\n-----[ Menu ]-----");
                             System.out.println("1 - Depositar");
                             System.out.println("2 - Sacar");
-                            System.out.println("3 - Ver saldo");
-                            System.out.println("4 - Transeferir");
-                            System.out.println("5 - Sair");
+                            System.out.println("3 - Transeferir");
+                            System.out.println("4 - Sair");
                             System.out.print("Digite: ");
+                            try {
                                 escolha = scanf.nextInt();
+                            } catch (InputMismatchException e) {
+                                System.out.println("\nErro: O valor digitado não corresponde a um número.\n");
+                            } finally {
                                 scanf.nextLine();
+                            }
                             switch (escolha) {
                                 case 1:
-                                    System.out.print("Qual o valor?\nR: ");
+                                    System.out.print("\nQual o valor?\nR: ");
                                         valor = scanf.nextInt();
                                         scanf.nextLine();
                                     dono.deposita(valor);
                                     break;
                                 case 2:
                                     try {
-                                        System.out.print("Qual o valor?\nR: ");
+                                        System.out.print("\nQual o valor?\nR: ");
                                             valor = scanf.nextInt();
                                             scanf.nextLine();
                                         dono.saca(valor);
@@ -70,22 +89,24 @@ public class SistemaBanco {
                                     }
                                     break;
                                 case 3:
-                                    System.out.println("Saldo: " + dono.getSaldo());
-                                    break;
-                                case 4:
-                                    System.out.print("Qual o valor?\nR: ");
+                                    System.out.print("\nQual o valor?\nR: ");
                                         valor = scanf.nextInt();
-                                    System.out.print("Qual o codigo da conta a transferir?\nR: ");
+                                    System.out.print("\nQual o codigo da conta a transferir?\nR: ");
                                         codigo = scanf.nextInt();
                                         scanf.nextLine();
-                                    dono.transfere(valor, banco.procuraConta(codigo));
-                                case 5:
+                                    try {
+                                        dono.transfere(valor, banco.procuraConta(codigo));    
+                                    } catch (ContaInexistente e) {
+                                        System.out.println(e.getMessage());
+                                    }
+                                    break;
+                                case 4:
                                     break;
                                 default:
                                     System.out.println("\t[Tente novamente!]");
                                     break;
                             }
-                        } while (escolha != 5);
+                        } while (escolha != 4);
                     } catch (ContaInexistente e) {
                         System.out.println(e.getMessage());
                     }
@@ -94,7 +115,7 @@ public class SistemaBanco {
                     scanf.close();
                     return;
                 default:
-                    System.out.println("\t[Tente novamente!]");
+                    System.out.println("\n\t[Tente novamente!]");
                     break;
             } 
         } while (true);
